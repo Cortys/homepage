51ad0b8d85b97aa7e9314af88144290d776b2aed
Clemens Damke <admin@cortys.de>
2018-09-09 15:26:14 +0200

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd "${BASH_SOURCE%/*}" || exit
+
+eval $(cat .ftpenv)
+
+echo "Rebuilding site..."
+
+if npm run build; then
+	echo "Rebuilt site!"
+else
+	echo "Build error."
+	exit 1
+fi
+
+echo "Deploying site..."
+
+if lftp -c "open --password $PASSWORD sftp://$USER@$HOST:$PORT; mirror -c -e -R -L ./dist $TARGET"; then
+	echo "Page deployed!"
+else
+	echo "Error while deploying."
+	exit 1
+fi
